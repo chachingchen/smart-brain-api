48f430fc7fa9ffc69b177d7c00e4c1dbbb04e456
Gina.Chen/陳靓 <Gina.Chen@JMC.LOCAL>
2021-07-28 17:14:33 +0800

    1.signIn, register cannot empty 2. move api key to server

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,17 @@
+const clarifai =  require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '7944ea3a4d1744539ca18a6f69c49630'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict('e466caa0619f444ab97497640cefc4dc', req.body.input)
+		.then(data => {
+		  res.json(data);	
+		})
+		.catch(err => res.status(400).json('unable to work with API'))
+}
 
 const handleImage = (req, res, db) => {
 	const {id} = req.body;
@@ -10,4 +24,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
   handleImage
+  handleApiCall
 }
