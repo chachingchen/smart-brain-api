db0c842feaeb717c1946f873ef6b3f03b7ab4e77
m0611104 <m0611104@gcloud.csu.edu.tw>
2021-08-01 14:28:24 +0800

    updating db host

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-animated-02045',
     user : 'postgres',
     password : '09255432',
     database : 'smart-brain'
