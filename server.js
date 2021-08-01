5876e4e8e8d70d28cae6a8f170462b3b2816deb9
m0611104 <m0611104@gcloud.csu.edu.tw>
2021-08-01 17:30:01 +0800

    repeat adding db conn v2

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    coonectionString : process.env.DATABASE_URL,
     ssl: true,
   }
 });
