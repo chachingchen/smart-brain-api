72b9b150e1a18e0bd9402e345d9e17f1c80bedc3
Gina.Chen/陳靓 <Gina.Chen@JMC.LOCAL>
2021-08-19 14:09:20 +0800

    validate password length & email

--- controllers/register.js
+++ controllers/register.js
@@ -1,8 +1,16 @@
+var validator = require('validator');
 
 const handleRegister= (req, res, db, bcrypt) => {
 	const {email, name, password} = req.body;
 	if (!email || !name || !password) {
 		return res.status(400).json('incorrect form submission');
+	}
+	if (password.length < 4) {
+		return res.status(400).json('password length should be 4 characters or longer');
+	}
+
+	if (!validator.isEmail(email)) {
+		return res.status(400).json('invalid Email');
 	}
 	const hash = bcrypt.hashSync(password);
 		db.transaction(trx => {
